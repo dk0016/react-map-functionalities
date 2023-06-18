8e7bff465e5754701d166b8333b71da663ff4148
Dinesh Kumar J <dineshjayakumar16@gmil.com>
2023-06-18 13:32:20 +0530

    pdf page rotate

--- src/components/Routes/index.js
+++ src/components/Routes/index.js
@@ -2,16 +2,18 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import App from "../../App";
 import Common from "../Common";
-import FormControl from "../Form";
+// import FormControl from "../Form";
 import MultipleFilter from "../MultipleFilter";
+import Document from "../Document";
 const Routing = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/accorion" element={<App />} />
-        <Route path="/" element={<FormControl />} />
+        {/* <Route path="/" element={<FormControl />} /> */}
+        <Route path="/" element={<Document />} />
         <Route path="/common" element={<Common />} />
-        <Route path='/multiplefilter' element={<MultipleFilter />}/>
+        <Route path="/multiplefilter" element={<MultipleFilter />} />
       </Routes>
     </BrowserRouter>
   );
